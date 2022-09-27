e1b992c04c16aeb07e7196205e66e04372ff175a
Alexander Davis <nameindustry@gmail.com>
2022-09-27 15:28:48 -0400

    [ update ] - fix font includes
    quick edit to check @fontsource css files matching tailwinds font-weights

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React, { Suspense } from "react";
 import { createRoot } from 'react-dom/client';
 
-// Fonts supported 300, 400, 500, 600, 700, 800
+// @fontsource default support is 300, 400, 500, 600, 700, 800 ( w/Italics )
 // Include only required.
-import "@fontsource/open-sans"; // 400
-// import "@fontsource/open-sans/600.css";
-// import "@fontsource/open-sans/700.css";
-// import "@fontsource/open-sans/800.css";
+// ------------------------------------------
+
+import "@fontsource/open-sans";
+
+// 100 is faux thin - TailwindCSS font-black
+// import "@fontsource/open-sans/300.css"; // TailwindCSS font-light
+// import "@fontsource/open-sans/400.css"; // TailwindCSS font-normal
+// import "@fontsource/open-sans/500.css"; // TailwindCSS font-medium 
+// import "@fontsource/open-sans/600.css"; // TailwindCSS font-semibold
+// import "@fontsource/open-sans/700.css"; // TailwindCSS font-bold
+// import "@fontsource/open-sans/800.css"; // TailwindCSS font-extrabold
+// 900 is faux black - TailwindCSS font-black
 
 import "./index.css";
 
